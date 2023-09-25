21e6caa94719da48ce0c3093cc2cdd08086a1494
Abdullah Binsheheween <s202221520@ccm.kfupm.edu.sa>
2023-09-25 15:36:22 +0300

    Insertion sort

--- Sorter.java
+++ Sorter.java
@@ -2,4 +2,18 @@ public class Sorter {
     public static void main(String[] args) {
         return;
     }
+
+    public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
+            }  
+            array[i+1] = key;  
+        }  
+    }  
+    
 }
